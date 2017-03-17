5920848173bd869b27a945509fcf0f23e381d54d
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-03-17 17:41:32 +0100

    add layout home

---
+++ Labeling/app/src/main/java/com/lorusso/luca/labeling/Mode.java
@@ -0,0 +1,16 @@
+package com.lorusso.luca.labeling;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode);
+        getIntent();
+
+    }
+}
