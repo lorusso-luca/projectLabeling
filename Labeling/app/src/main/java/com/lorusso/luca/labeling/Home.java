8caa6f13ef35858ed3448565f8a23a818ccfc9c7
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-03-20 11:11:28 +0100

    add activity Mode

--- Labeling/app/src/main/java/com/lorusso/luca/labeling/Home.java
+++ Labeling/app/src/main/java/com/lorusso/luca/labeling/Home.java
@@ -6,30 +6,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 
 
 public class Home extends AppCompatActivity {
     EditText id;
-    Button login;
+    Button loginID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         id = (EditText) findViewById(R.id.idText);
-        login = (Button) findViewById(R.id.loginButton);
+        loginID = (Button) findViewById(R.id.buttonLogin);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        loginID.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String idUser = id.getText().toString();
-                Intent i = new Intent(Home.this,Mode.class);
+                Intent i = new Intent(Home.this, Mode.class);
+                i.putExtra("idUser",idUser);
 
-               // SharedPreferences sharedpreferences = getSharedPreferences("User", Context.MODE_WORLD_READABLE);
-                //SharedPreferences.Editor editor = sharedpreferences.edit();
-                //editor.putString("key", idUser);
-                //editor.commit();
 
                 startActivity(i);
 
--- Labeling/app/src/main/java/com/lorusso/luca/labeling/Mode.java
+++ Labeling/app/src/main/java/com/lorusso/luca/labeling/Mode.java
@@ -1,16 +1,63 @@
 package com.lorusso.luca.labeling;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Mode extends AppCompatActivity {
+    TextView prova;
+    RadioButton radioLibero;
+    RadioButton radioVincolato;
+    Button buttonMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
-        getIntent();
+        radioLibero = (RadioButton) findViewById(R.id.radioLibero);
+        radioVincolato = (RadioButton) findViewById(R.id.radioVincolato);
+        buttonMode = (Button) findViewById(R.id.buttonMode);
+        Intent i = getIntent();
 
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radioVincolato:
+                if (checked)
+                    buttonMode.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+
+                            Intent i = new Intent(Mode.this, Home.class);
+
+                            startActivity(i);
+
+                        }
+                    });
+
+            case R.id.radioLibero:
+                if (checked)
+                    buttonMode.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+
+                            Intent i = new Intent(Mode.this, Home.class);
+
+                            startActivity(i);
+
+                        }
+                    });
+
+        }
     }
 }
