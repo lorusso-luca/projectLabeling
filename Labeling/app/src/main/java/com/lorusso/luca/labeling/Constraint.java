9e9ba4c0c00c4b367040ecdfcf24e83f4320350d
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-03-20 18:02:45 +0100

    add raw folder

--- Labeling/app/src/main/java/com/lorusso/luca/labeling/Constraint.java
+++ Labeling/app/src/main/java/com/lorusso/luca/labeling/Constraint.java
@@ -1,13 +1,57 @@
 package com.lorusso.luca.labeling;
 
+import android.content.res.Resources;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+
 public class Constraint extends AppCompatActivity {
-Protocol protocol;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint);
+
+
+        JSONParser parser = new JSONParser();
+
+
+        try {
+            Resources res = getResources();
+            InputStream in_s = res.openRawResource(R.raw.protocolli);
+
+            byte[] b = new byte[in_s.available()];
+            in_s.read(b);
+
+
+            Object obj = parser.parse(String.valueOf(in_s));
+            JSONArray protocolli = (JSONArray) obj;
+            File file = Environment.getDataDirectory().getAbsoluteFile();
+            int id = protocolli.size();
+
+          //  String name = (String) jsonObject.get("Name");
+           // String author = (String) jsonObject.get("Author");
+            //JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- Labeling/app/src/main/java/com/lorusso/luca/labeling/Protocol.java
+++ Labeling/app/src/main/java/com/lorusso/luca/labeling/Protocol.java
@@ -51,5 +51,23 @@ public class Protocol {
                 '}';
     }
 
+    public ArrayList<Protocol> createProtocol (){
+        String id = "1";
+        String nome = "esempio";
+        String descrizione = "Protocollo dedicato alla camminata e seduta";
+        Exercise e1 = new Exercise("camminata",10);
+        Exercise e2 = new Exercise("seduti", 20);
+        ArrayList<Exercise> e = new ArrayList<Exercise>();
+        e.add(e1);
+        e.add(e2);
+        Protocol p1 = new Protocol(id,nome,descrizione,e);
+        ArrayList<Protocol> p = new ArrayList<Protocol>();
+        p.add(p1);
+
+
+
+        return p;
+    }
+
 
 }
