a77395d42c448852250274250962b8ee6ca70281
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-03-29 18:19:18 +0200

    add free Activity with widgets

--- Labeling/app/src/main/java/com/lorusso/luca/labeling/Free.java
+++ Labeling/app/src/main/java/com/lorusso/luca/labeling/Free.java
@@ -2,18 +2,32 @@ package com.lorusso.luca.labeling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Free extends AppCompatActivity {
+    Button starFree;
+    Button stopFree;
+    Button completeFree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        starFree = (Button) findViewById(R.id.buttonStart);
+
+
+        stopFree = (Button) findViewById(R.id.buttonStop);
+
+
+        completeFree = (Button) findViewById(R.id.buttonCompleteFree);
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+
     }
 }
